9719c8dbc92835daa920a0437b592e1c79cdd596
Mike Kaplan <mike@mikekaplan.net>
2013-07-22 05:27:27 +0000

    Re-added files necessary for heroku app creation.

---
+++ web.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+var fs = require('fs');
+
+app.get('/', function(request, response) {
+  response.send(fs.readFileSync('index.html').toString());
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
